f5d6cca43645b8239fad8aef183e9685c796819f
tdonohoe <tdonohoe@uchicago.edu>
2023-05-11 21:59:56 -0500

    removed last two extaneous files

--- reading_excel.js
+++
@@ -1,6 +0,0 @@
-import { d3 } from './common.js';
-d3.csv('sample_csv.csv').then((data) => {
-    /* these are called after the data is read, which happens asynchronously (the .then callback
-    is called when the promise of the data read has resolved) */
-    console.log(data);
-  });
